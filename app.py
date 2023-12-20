6450db51c90573db9e8ccf80ee258cb4e37d9e3e
Pablo Batista <pabloreis2001@gmail.com>
2023-12-20 18:10:56 -0300

    att

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__, static_url_path='/static')
 def login():
     return render_template('html/register/login.html')
 
+@app.route('/register.html')
+def register():
+    return render_template('html/register/register.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
