4cb506eed9bf7f851d757023340ca6a1520d77ed
Christian <christianlouzada.castro@gmail.com>
2023-12-21 21:09:31 -0300

    feat(index.js): Inclusão do efeito de escrever nas páginas de registro

---
+++ static/js/index.js
@@ -0,0 +1,34 @@
+
+const title = document.querySelector('[g-id=title]')
+const redirection = document.querySelector('[g-id=redirection]')
+
+const typeWhriter = (title, redirection)=> {
+    try {
+        
+        const textArrayTitle = title.innerHTML.split('');
+        title.innerHTML = '';
+
+        const textArrayRedirection = redirection.innerHTML.split('');
+        redirection.innerHTML = '';
+
+        textArrayTitle.forEach((letter, i)=> {
+
+            setTimeout(function(){
+                title.innerHTML += letter
+            }, 190 * i)
+        });
+
+        textArrayRedirection.forEach((letter, i)=> {
+
+            setTimeout(function(){
+                redirection.innerHTML += letter
+            }, 140 * i)
+        });
+
+
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+
+typeWhriter(title, redirection)
