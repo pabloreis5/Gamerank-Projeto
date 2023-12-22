8631f5d199e9c0d102884d614f4bf1184cff983e
Matheus Azevedo <matheus.azevedo@aluno.ufop.edu.br>
2023-12-22 18:39:49 -0300

    Complementando o template base

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def login():
     return render_template('html/register/login.html')
+
+@app.context_processor
+def inject_active():
+    def is_active(endpoint): #recebe o endpoint (rota) e verifica se é igual o endpoint atual, se for ele retorna a classe active, se não retorna vazio
+        return 'active' if request.endpoint == endpoint else ''
+    return dict(is_active=is_active)
 
 @app.route('/register')
 def register():
